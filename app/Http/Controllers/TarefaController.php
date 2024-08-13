82a318c8186196b070e215612bdb2f36c545103b
matheus.diniz <matheusv.diniz95@gmail.com>
2024-08-13 14:30:29 -0300

    feat: blade de create e show de tarefas e metodo de receber email a cada tarefa cadastrada

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\NovaTarefaMail;
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class TarefaController extends Controller
 {
    public function __construct(){
-    // $this->middleware('auth');
+     $this->middleware('auth');
    }
 
     public function index()
@@ -22,67 +24,40 @@ class TarefaController extends Controller
         
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function create()
     {
-        //
+        return view('tarefa.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
-        //
+        $tarefa = Tarefa::create($request->all());
+        $destinatario = auth()->user()->email;
+        Mail::to($destinatario)->send(new NovaTarefaMail($tarefa));
+        return redirect()->route('tarefa.show',['tarefa' => $tarefa->id]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Tarefa  $tarefa
-     * @return \Illuminate\Http\Response
-     */
+  
     public function show(Tarefa $tarefa)
     {
-        //
+       return view('tarefa.show',['tarefa' => $tarefa]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Tarefa  $tarefa
-     * @return \Illuminate\Http\Response
-     */
+  
     public function edit(Tarefa $tarefa)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Tarefa  $tarefa
-     * @return \Illuminate\Http\Response
-     */
+  
     public function update(Request $request, Tarefa $tarefa)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Tarefa  $tarefa
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function destroy(Tarefa $tarefa)
     {
         //
